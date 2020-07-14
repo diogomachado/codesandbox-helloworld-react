0d3ee0b2f887f129e5257cb6bebb1fd8ab2bb4cc
Diogo Machado <diogo@diogomachado.com>
2020-07-13 22:15:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>
+        This is the famous phrase in the world computer. I`m just another
+        development testing a new tecnology.
+      </h2>
+      <p>By: Diogo Machado</p>
+    </div>
+  );
+}
