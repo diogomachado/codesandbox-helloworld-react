1f2f6a4db65550023e36de932da28062a313b77e
Diogo Machado <diogo@diogomachado.com>
2020-07-14 14:49:29 -0300

    Fix phrase

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h2>
-        This is the famous phrase in the world computer. I`m just another
-        development testing a new tecnology.
+        This is the famous phrase in the computer world. I`m just another
+        development testing a new technology.
       </h2>
       <p>By: Diogo Machado</p>
     </div>
